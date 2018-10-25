4e0e1c12ba23c3529e0eb4cd22622ff720b34492
Tiffany Chien <37984481+tiffanyxchien@users.noreply.github.com>
2018-10-24 20:12:21 -0700

    Add files via upload

--- timeseries.js
+++ timeseries.js
@@ -20,48 +20,100 @@ function handleSubmit() {
   buildPlot(state);
 }
 
-function buildPlot(state) {
-  var apiKey = "za-7uUfzKfhsCkHJSzQ-";
+function init(){
 
+  var apiKey = "za-7uUfzKfhsCkHJSzQ-";
+  var states = [13,6,21,47,7];
+  var url;
+  for (i in states){
+    url = `https://www.quandl.com/api/v3/datasets/ZILLOW/S${states[i]}_ZHVIAH?api_key=${apiKey}`;
+    d3.json(url).then(function(data) {
+
+      // Grab values from the response json object to build the plots
+      var name = data.dataset.name;
+      var state_name = name.split("-")[2]
+      console.log(state_name)
+      var startDate = data.dataset.start_date;
+      var endDate = data.dataset.end_date;
+      var dates = unpack(data.dataset.data, 0);
+      var homeValue = unpack(data.dataset.data, 1);
+      var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: state_name,
+        x: dates,
+        y: homeValue,
+        line: {
+          color: states[i]
+        }
+      };
+  
+      var data = [trace1];
+  
+      var layout = {
+        title: "Top 5 States Home Value Time Series",
+        width: 1000,
+        height: 600,
+        xaxis: {
+          range: [startDate, endDate],
+          type: "date"
+        },
+        yaxis: {
+          autorange: true,
+          type: "linear"
+        }
+      };
+  
+      Plotly.plot("plot", data, layout);
+  
+    });
+
+  }
+}
+
+function updatePlotly(dates, homeValue, state_name, name) {
+
+  var CHART = document.getElementById("plot");
+
+  var newLayout  = {
+    title: `${state_name} Housing Prices Over Time`,
+    width: 1000,
+    height: 600
+  };
+
+  var newData = {
+    x: [dates],
+    y: [homeValue],
+    name: [state_name]};
+
+  Plotly.deleteTraces(CHART, 0);
+  Plotly.deleteTraces(CHART, 0);
+  Plotly.deleteTraces(CHART, 0);
+  Plotly.deleteTraces(CHART, 0);
+  Plotly.update(CHART, newData, newLayout)
+};
+
+function buildPlot(state) {
+
+  var apiKey = "za-7uUfzKfhsCkHJSzQ-";
   var url = `https://www.quandl.com/api/v3/datasets/ZILLOW/S${state}_ZHVIAH?api_key=${apiKey}`;
 
   d3.json(url).then(function(data) {
 
     // Grab values from the response json object to build the plots
     var name = data.dataset.name;
+    var state_name = name.split("-")[2]
     var startDate = data.dataset.start_date;
     var endDate = data.dataset.end_date;
     var dates = unpack(data.dataset.data, 0);
     var homeValue = unpack(data.dataset.data, 1);
 
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      x: dates,
-      y: homeValue,
-      line: {
-        color: "#17BECF"
-      }
-    };
-
-    var data = [trace1];
-
-    var layout = {
-      title: name,
-      xaxis: {
-        range: [startDate, endDate],
-        type: "date"
-      },
-      yaxis: {
-        autorange: true,
-        type: "linear"
-      }
-    };
-
-    Plotly.newPlot("plot", data, layout);
+    updatePlotly(dates, homeValue, state_name, name)
 
   });
 }
+
+init();
 
 // Add event listener for submit button
 d3.select("#submit").on("click", handleSubmit);
