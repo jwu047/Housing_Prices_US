4cb9e4e410c274f50ae8d46ad946137c250f9535
Jason Wu <jwu047@ucr.edu>
2018-10-27 00:13:58 -0700

    approutes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_pymongo import PyMongo
 import news_scrap
 
@@ -8,13 +8,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mortgage_app"
 mongo = PyMongo(app)
 
-@app.route("/")
-def home():
-    news_scrap = mongo.db.collection.find_one()
-    return render_template("home.html", news_scrap=news_scrap)
+@app.route("/", methods = ['GET', 'POST'])
+@app.route("/index", methods = ['GET', 'POST'])
+def index():
+    #if request.method =='POST':
+    #    return redirect(url_for('index'))
+    #else:
+        news_scrap = mongo.db.collection.find_one()
+        return render_template("index.html", news_scrap=news_scrap)
 
 
-@app.route("/scrap")
+@app.route("/scrap", methods = ['GET', 'POST'])
 def scraper():
 
     news_info = mongo.db.collection
@@ -22,6 +26,26 @@ def scraper():
     news_info.update({}, news_data, upsert=True)
     return redirect("/", code=302)
 
+@app.route("/dataexploration", methods = ['GET', 'POST'])
+def dataexploration():
+	return render_template("dataexploration.html")
+
+@app.route("/mortgate-rate-table", methods = ['GET', 'POST'])
+def ratetable():
+	return render_template("mortgage_rate_table.html")
+
+
+@app.route("/price-factors", methods = ['GET', 'POST'])
+def pricefactor():
+	return render_template("price_factors.html")
+
+@app.route("/timeseries", methods = ['GET', 'POST'])
+def timeseries():
+	return render_template("timeseries.html")
+
+@app.route("/leaflet", methods = ['GET', 'POST'])
+def leaflet():
+    return render_template("leaflet.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
